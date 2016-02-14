8427907b0a56dd279ba4c1f66a5c47468bf65237
dzyomaArtem <dz.art.23@gmail.com>
2016-02-15 00:51:18 +0200

    Added user's email to posts and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,10 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment].permit(:name, :body))
+    comment = @post.comments.create(comment_params)
+    comment.user = current_user
 
-    redirect_to post_path(@post)
+    redirect_to post_path @post if comment.save
   end
 
   def destroy
@@ -12,5 +13,11 @@ class CommentsController < ApplicationController
     @comment.destroy
 
     redirect_to post_path(@post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
